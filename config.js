7ce067a083194f03e6d55943cd314c4ecd3bf40f
Andrey Sagurov <9624294@gmail.com>
2025-11-07 21:05:12 +0300

    feat: initial admin panel setup

--- config.js
+++ config.js
@@ -1,22 +1,17 @@
 window.ADMIN_CONFIG = {
-    // Настройки репозитория
     repo: {
         owner: 'Andreyhiitola',
         repo: 'pelikan-app', 
         branch: 'main',
         contentPath: 'shared-content/data'
     },
-    
-    // Типы контента
     contentTypes: ['activities', 'news', 'gallery', 'menu', 'weather', 'offers'],
-    
-    // Названия для интерфейса
     names: {
-        activities: 'Активности',
+        activities: 'Активности', 
         news: 'Новости', 
         gallery: 'Галерея',
-        menu: 'Меню',
-        weather: 'Погода',
+        menu: 'Меню', 
+        weather: 'Погода', 
         offers: 'Предложения'
     }
 };
